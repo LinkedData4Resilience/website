0c431b9162b4c8491ae51c2cec83ed0ae0d2561a
Shuai Wang <shuaiwang@Shuais-Air.fritz.box>
2023-09-22 00:27:43 +0200

    try again

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -69,8 +69,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "hello",
     "bootstrap4",
-    "hello"
 ]
 
 MIDDLEWARE = [
